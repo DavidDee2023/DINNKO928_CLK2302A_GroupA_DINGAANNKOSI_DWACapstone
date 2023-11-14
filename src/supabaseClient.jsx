05e8c4aa86f9864d11ae5a928ad0fdd4e7486b39
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-11-14 13:06:42 +0200

    Modifed God App

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,5 +1,7 @@
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
+
 const supabaseUrl = 'https://czhvpwzghqnmaelfnetk.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6aHZwd3pnaHFubWFlbGZuZXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk0NzA2NjIsImV4cCI6MjAxNTA0NjY2Mn0.vxNZzNBZl9pU3rMqAYzYP7_BSc1N049dKkuPpBJVqNc"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
