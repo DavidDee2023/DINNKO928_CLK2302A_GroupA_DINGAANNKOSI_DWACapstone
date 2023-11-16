a01f06a00091f3bae1aa5153aabace38a6dd56d8
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-11-16 12:00:26 +0200

    Modified files

--- src/components/SinglePodcastDetails/SinglePodcastDetails.jsx
+++ src/components/SinglePodcastDetails/SinglePodcastDetails.jsx
@@ -80,8 +80,8 @@ export default function SinglePodcastDetails({
       <div className="loading--icon">
         <ColorRing
           visible={true}
-          height="150"
-          width="150"
+          height="100"
+          width="100"
           ariaLabel="blocks-loading"
           wrapperStyle={{}}
           wrapperClass="blocks-wrapper"
